6a1132c2553c99f5542fdb4f130fdd1519305b77
Ilyass Taouil <itaouil95@gmail.com>
2017-05-11 20:25:20 +0200

    Implemented gulp task runner file.

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// Modules
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var nodemon     = require('gulp-nodemon');
+var browserSync = require('browser-sync');
+
+// Live reloading
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:3000/",
+    files: ["public/**/*.*"],
+    browser: "google chrome",
+    port: 7000
+  });
+});
+
+// Nodemon task
+gulp.task('nodemon', function(cb) {
+
+  var started = false;
+
+  return nodemon({
+    script: './bin/www'
+  }).on('start', function() {
+    // Avoid multiple restart
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+
+});
+
+// Compile SASS files on change
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+  gulp.watch('public/sass/style.sass', ['sass']);
+  gulp.watch('views/*.pug', browserSync.reload);
+  gulp.watch('public/js/*.js', browserSync.reload);
+});
+
+// Preprocessing SASS files
+gulp.task('sass', function(){
+  return gulp.src('public/sass/style.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
